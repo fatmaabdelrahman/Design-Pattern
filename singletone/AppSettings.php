f985286669bf09ea8ae413d4f4135dc773da6f4e
fatma123 <fabdelrahman72@yahoo.com>
2021-04-03 22:43:40 +0200

    finish single tone

--- singletone/AppSettings.php
+++ singletone/AppSettings.php
@@ -5,7 +5,9 @@ class AppSettings
     private static ?self $instance = null;
     private static array $config;
 
-    private function __construct(){}
+    public function __construct(){
+        self::$config = parse_ini_file(__DIR__.'/config.ini', true);
+    }
 
     private function __clone(){}
 
@@ -24,7 +26,17 @@ class AppSettings
 
 }
 
-$db= AppSettings::getInstance();
-var_dump($db);
+// 1- $db=  new AppSettings(); => error because construct is error
+$database= AppSettings::getInstance();
+$database2= AppSettings::getInstance();
+//var_dump($database::getConfig('Database'));
+//var_dump($database::getConfig('Database')['portNumber']);
+//var_dump($database::getConfig('Cache'));
 
+var_dump($database, $database2);
+// the same instance
+
+
+// self refer to current class and  PHP self refers to the class members, but not for any particular object.
+// This is because the static members(variables or functions) are class members shared by all the objects of the class.
 
